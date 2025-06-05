c9abf1b97a10ce895fbea617e7d30f14d343c586
nsamuels <n-samuels@outlook.com>
2025-06-05 16:11:15 +0100

    updated Favourite model entity

--- src/main/java/com/example/RecipeApp/models/Favourite.java
+++ src/main/java/com/example/RecipeApp/models/Favourite.java
@@ -1,4 +1,42 @@
 package com.example.RecipeApp.models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Favourite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    Favourite -> User
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "meal_id")
+    private Meal meal;
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public User getUser(){
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public Meal getMeal(){
+        return meal;
+    }
+
+    public void setMeal(Meal meal){
+        this.meal = meal;
+    }
 }
