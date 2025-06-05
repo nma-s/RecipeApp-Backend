fa9c6d77da4c93fe0bbd44d1fb3a123950c47886
nsamuels <n-samuels@outlook.com>
2025-06-05 13:58:51 +0100

    Updated User entity model file with fields, getters and setters

--- src/main/java/com/example/RecipeApp/models/User.java
+++ src/main/java/com/example/RecipeApp/models/User.java
@@ -1,4 +1,53 @@
 package com.example.RecipeApp.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
+
+//    User -> Favourite
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Favourite> favourites;
+
+//    Methods
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public List<Favourite> getFavourites(){
+        return favourites;
+    }
+
+    public void setFavourites(List<Favourite> favourites) {
+        this.favourites = favourites;
+    }
 }
